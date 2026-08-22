f0df928227d92b37c755b361013d8ccb85d31d35
knarf22 <gonzalesjosefrank22@gmail.com>
2026-08-23 03:44:14 +0800

    Update ParkingRatesController.cs

--- ParkingSystem.Api/ParkingSystem.Api/Controllers/ParkingRatesController.cs
+++ ParkingSystem.Api/ParkingSystem.Api/Controllers/ParkingRatesController.cs
@@ -1,12 +1,102 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkingSystem.Api.Data;
+using ParkingSystem.Api.Models;
 
 namespace ParkingSystem.Api.Controllers
 {
-    public class ParkingRatesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ParkingRatesController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ParkingDbContext _context;
+
+        public ParkingRatesController(ParkingDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: api/ParkingRates
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ParkingRate>>> GetParkingRates()
+        {
+            return await _context.ParkingRates.ToListAsync();
+        }
+
+        // GET: api/ParkingRates/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ParkingRate>> GetParkingRate(int id)
+        {
+            var parkingRate = await _context.ParkingRates.FindAsync(id);
+
+            if (parkingRate == null)
+            {
+                return NotFound();
+            }
+
+            return parkingRate;
+        }
+
+        // POST: api/ParkingRates
+        [HttpPost]
+        public async Task<ActionResult<ParkingRate>> CreateParkingRate(
+            ParkingRate parkingRate)
+        {
+            _context.ParkingRates.Add(parkingRate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetParkingRate),
+                new { id = parkingRate.Id },
+                parkingRate
+            );
+        }
+
+        // PUT: api/ParkingRates/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateParkingRate(
+            int id,
+            ParkingRate parkingRate)
+        {
+            if (id != parkingRate.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(parkingRate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await _context.ParkingRates.AnyAsync(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/ParkingRates/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteParkingRate(int id)
+        {
+            var parkingRate = await _context.ParkingRates.FindAsync(id);
+
+            if (parkingRate == null)
+            {
+                return NotFound();
+            }
+
+            _context.ParkingRates.Remove(parkingRate);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
