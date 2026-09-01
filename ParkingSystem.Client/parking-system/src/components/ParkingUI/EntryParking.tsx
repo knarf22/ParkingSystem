6056046ac8d6ab2bae7c71c26567e372c725db99
knarf22 <gonzalesjosefrank22@gmail.com>
2026-09-01 20:19:54 +0800

    Update EntryParking.tsx

--- ParkingSystem.Client/parking-system/src/components/ParkingUI/EntryParking.tsx
+++ ParkingSystem.Client/parking-system/src/components/ParkingUI/EntryParking.tsx
@@ -46,7 +46,7 @@ const EntryParking = () => {
                 Parking Entry
             </h3>
 
-            <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2">
+            <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-3">
 
                 {/* Plate Number */}
                 <div>
@@ -69,16 +69,9 @@ const EntryParking = () => {
                         Vehicle Type
                     </label>
 
-                    <div className="relative max-w-sm">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" /></svg>
-                        </div>
-                        <input
-                            type="date"
-                            id="default-datepicker"
-                            className="block w-full ps-9 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5"
-                            placeholder="Select date"
-                        />
+
+                    <div>
+
                     </div>
 
 
@@ -93,6 +86,22 @@ const EntryParking = () => {
                         <option value="Motorcycle">Motorcycle</option>
                         <option value="Van">Van</option>
                     </select>
+                </div>
+                <div>
+                    <label className="mb-2 block text-sm font-medium text-gray-700">
+                        Select Date
+                    </label>
+                    <div className="relative max-w-sm">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" /></svg>
+                        </div>
+                        <input
+                            type="date"
+                            id="default-datepicker"
+                            className="w-full rounded-lg  border-gray-300 ps-9 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5"
+                            placeholder="Select date"
+                        />
+                    </div>
                 </div>
             </div>
 
