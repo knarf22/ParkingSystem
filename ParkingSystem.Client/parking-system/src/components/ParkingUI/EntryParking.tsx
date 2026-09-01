f5807a548a32fd75288d89d0f2669013814f00c8
knarf22 <gonzalesjosefrank22@gmail.com>
2026-09-01 20:30:38 +0800

    Update EntryParking.tsx

--- ParkingSystem.Client/parking-system/src/components/ParkingUI/EntryParking.tsx
+++ ParkingSystem.Client/parking-system/src/components/ParkingUI/EntryParking.tsx
@@ -5,6 +5,7 @@ const EntryParking = () => {
 
     const [plateNumber, setPlateNumber] = useState("");
     const [vehicleType, setVehicleType] = useState("");
+    const [selectedDate, setSelectedDate] = useState("");
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
 
@@ -25,11 +26,13 @@ const EntryParking = () => {
             const res = await api.post("/Parking",
                 {
                     plateNumber: plateNumber.trim(),
-                    vehicleType: vehicleType
+                    vehicleType: vehicleType,
+                    entryTime : selectedDate
                 })
             console.log(res.data)
             setPlateNumber("");
             setVehicleType("");
+            setSelectedDate("");
         } catch (error: any) {
             setError(error.response.data || "failed to park")
         } finally {
@@ -42,6 +45,7 @@ const EntryParking = () => {
 
     return (
         <div className="rounded-xl border border-gray-200 bg-white p-6 shadow-sm">
+            {JSON.stringify(selectedDate)}
             <h3 className="text-lg font-semibold text-gray-800">
                 Parking Entry
             </h3>
@@ -68,13 +72,6 @@ const EntryParking = () => {
                     <label className="mb-2 block text-sm font-medium text-gray-700">
                         Vehicle Type
                     </label>
-
-
-                    <div>
-
-                    </div>
-
-
                     <select
                         value={vehicleType}
                         onChange={(e) => setVehicleType(e.target.value)}
@@ -96,10 +93,16 @@ const EntryParking = () => {
                             <svg className="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" /></svg>
                         </div>
                         <input
-                            type="date"
-                            id="default-datepicker"
+                            // type="date"
+                            // id="default-datepicker"
+                            type="datetime-local"
+                            id="entry-time"
                             className="w-full rounded-lg  border-gray-300 ps-9 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5"
                             placeholder="Select date"
+                            onChange={(e) => {
+                                setSelectedDate(e.target.value)
+                                e.target.blur()
+                            }}
                         />
                     </div>
                 </div>
