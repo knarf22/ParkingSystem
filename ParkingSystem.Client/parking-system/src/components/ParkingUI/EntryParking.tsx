4edb2feac6e547a10c5100d21b35df7284db7ad0
knarf22 <gonzalesjosefrank22@gmail.com>
2026-09-02 21:46:33 +0800

    Update EntryParking.tsx

--- ParkingSystem.Client/parking-system/src/components/ParkingUI/EntryParking.tsx
+++ ParkingSystem.Client/parking-system/src/components/ParkingUI/EntryParking.tsx
@@ -100,7 +100,7 @@ const EntryParking = () => {
                         Select Date
                     </label>
                     <div className="relative max-w-sm">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <div className="absolute inset-y-0 inset-s-0 flex items-center ps-3 pointer-events-none">
                             <svg className="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" /></svg>
                         </div>
                         <input
@@ -109,7 +109,7 @@ const EntryParking = () => {
                             // id="default-datepicker"
                             type="datetime-local"
                             id="entry-time"
-                            className="w-full rounded-lg  border-gray-300 ps-9 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5"
+                            className="w-full rounded-lg  border-gray-300 ps-9 pe-3 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5"
                             placeholder="Select date"
                             value={selectedDate}
                             onChange={(e) => {
